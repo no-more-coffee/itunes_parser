e70ecbfcd44b19a55e33ce15adf60568be15227b
no-more-coffee <nomorecoffee@yandex.ru>
2020-04-04 12:09:41 +0500

    Migrate tests to pytest.

--- tests/test.py
+++ tests/test.py
@@ -1,46 +1,54 @@
 import importlib.resources
-import unittest
 from xml.etree import ElementTree
+
+import pytest
 
 from parser.parser import parse_value, get_tracks, get_playlists
 from tests.fixtures.playlists import PLAYLISTS
 from tests.fixtures.tracks import TRACKS, TRACKS_WITH_NEWLINE_COMMENT
 
 
-class TestParse(unittest.TestCase):
-    def setUp(self) -> None:
-        xml_str = importlib.resources.read_text('tests.fixtures', 'All.xml')
-        root = ElementTree.fromstring(xml_str)
-        self.root_dict = root[0]
-
-    def test_simple_playlist(self):
-        data = parse_value(self.root_dict)
-        tracks = data.get('Tracks')
-        self.assertDictEqual(tracks, TRACKS)
-
-        playlists = data.get('Playlists')
-        self.assertListEqual(playlists, PLAYLISTS)
-
-    def test_only_tracks(self):
-        tracks_element = get_tracks(self.root_dict)
-        tracks = parse_value(tracks_element)
-        self.assertDictEqual(tracks, TRACKS)
-
-    def test_only_playlists(self):
-        playlists_element = get_playlists(self.root_dict)
-        playlists = parse_value(playlists_element)
-        self.assertListEqual(playlists, PLAYLISTS)
+def get_root_dict(xml_path):
+    xml_str = importlib.resources.read_text('tests.fixtures', xml_path)
+    root = ElementTree.fromstring(xml_str)
+    return root[0]
 
 
-class TestParseLineBreaks(unittest.TestCase):
-    def test_comments_with_line_breaks(self):
-        xml_str = importlib.resources.read_text('tests.fixtures', 'NewLineComments.xml')
-        root = ElementTree.fromstring(xml_str)
-        root_dict = root[0]
+@pytest.fixture
+def root_dict_all():
+    return get_root_dict('All.xml')
 
-        data = parse_value(root_dict)
-        tracks = data.get('Tracks')
-        self.assertDictEqual(tracks, TRACKS_WITH_NEWLINE_COMMENT)
 
-        playlists = data.get('Playlists')
-        self.assertListEqual(playlists, PLAYLISTS)
+def test_simple_playlist(root_dict_all):
+    data = parse_value(root_dict_all)
+    tracks = data.get('Tracks')
+    assert tracks == TRACKS
+
+    playlists = data.get('Playlists')
+    assert playlists == PLAYLISTS
+
+
+def test_only_tracks(root_dict_all):
+    tracks_element = get_tracks(root_dict_all)
+    tracks = parse_value(tracks_element)
+    assert tracks == TRACKS
+
+
+def test_only_playlists(root_dict_all):
+    playlists_element = get_playlists(root_dict_all)
+    playlists = parse_value(playlists_element)
+    assert playlists == PLAYLISTS
+
+
+@pytest.fixture
+def root_dict_newline_comments():
+    return get_root_dict('NewLineComments.xml')
+
+
+def test_comments_with_line_breaks(root_dict_newline_comments):
+    data = parse_value(root_dict_newline_comments)
+    tracks = data.get('Tracks')
+    assert tracks == TRACKS_WITH_NEWLINE_COMMENT
+
+    playlists = data.get('Playlists')
+    assert playlists == PLAYLISTS
